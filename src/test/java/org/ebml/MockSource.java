08465b05aa1c55203b7f20d2a9579ee049576643
Michael Jameson <michael.jameson@schneider-electric.com>
2015-02-09 15:25:11 -0800

    Adding MockSource used in new test

---
+++ src/test/java/org/ebml/MockSource.java
@@ -0,0 +1,65 @@
+package org.ebml;
+
+import java.nio.ByteBuffer;
+
+import org.ebml.io.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockSource implements DataSource
+{
+
+  private static final Logger LOG = LoggerFactory.getLogger(MockSource.class);
+  private final ByteBuffer buffer;
+
+  public MockSource(final ByteBuffer buffer)
+  {
+    this.buffer = buffer;
+  }
+
+  @Override
+  public long length()
+  {
+    return buffer.limit();
+  }
+
+  @Override
+  public long getFilePointer()
+  {
+    return buffer.position();
+  }
+
+  @Override
+  public boolean isSeekable()
+  {
+    return true;
+  }
+
+  @Override
+  public long seek(final long pos)
+  {
+    buffer.position((int) pos);
+    return pos;
+  }
+
+  @Override
+  public byte readByte()
+  {
+    return buffer.get();
+  }
+
+  @Override
+  public int read(final ByteBuffer buff)
+  {
+    buffer.get(buff.array(), buff.position(), buff.remaining());
+    buff.position(buff.limit());
+    return buff.remaining();
+  }
+
+  @Override
+  public long skip(final long offset)
+  {
+    buffer.position((int) (buffer.position() + offset));
+    return offset;
+  }
+}
